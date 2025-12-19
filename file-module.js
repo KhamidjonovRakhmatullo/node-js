833eadb3615b1d100fbf35e27b27c84ce27dd7a9
Rakhmatullo <richman.uzb1676@mail.ru>
2025-12-19 20:20:50 +0900

    Update file-module and add models module

--- file-module.js
+++ file-module.js
@@ -1,3 +1,48 @@
-console.log("Test file added to GitHub");
+const calculate = require("./models/hisob");
+const Account = require("./models/account");
 
-console.log("Test update file");
+/* ==================================================
+   CALCULATION MODULE USAGE
+   ================================================== */
+
+const natija = calculate.kopaytirish(3, 3);
+// console.log("Natija (kopaytirish):", natija);
+
+const natija2 = calculate.bolish(20, 2);
+// console.log("Natija (bolish):", natija2);
+
+const natija3 = calculate.qoshish(50, 50);
+// console.log("Natija (qoshish):", natija3);
+
+const natija4 = calculate.ayrish(22, 2);
+// console.log("Natija (ayrish):", natija4);
+
+
+
+/* ==================================================
+   ACCOUNT CLASS USAGE
+   ================================================== */
+
+// Call static method
+// Static methods belong to the class itself,
+// not to a specific account instance
+Account.tellMeAboutClass();
+Account.time();
+
+console.log("==========");
+
+// Create a new account instance
+// Parameters:
+// 1. Account holder name
+// 2. Initial balance
+// 3. Unique account ID
+const myAccount = new Account("Alan", 4000, "23d32-31dxw1-21-d-13d1-3d1");
+
+// Get account details
+myAccount.details();
+
+// Deposit to current balance
+myAccount.deposit(6000);
+
+// Withdraw from current balance
+myAccount.withdrawal(2000);
---
+++ models/account.js
@@ -0,0 +1,53 @@
+const moment = require("moment");
+
+
+class Account {
+  name;
+  #amount;
+  #account_id;
+
+  constructor(name, amount, account_id) {
+    this.name = name;
+    this.#amount = amount;
+    this.#account_id = account_id;
+  }
+
+  myBalance() {
+    console.log("Your balance:", this.#amount);
+    return this.#amount;
+  }
+
+  withdrawal(enteredAmount) {
+    if (this.#amount >= enteredAmount) {
+      this.#amount = this.#amount - enteredAmount;
+      console.log(
+        `Success! Your have withdrawn -${enteredAmount}$, and your balance is ${this.#amount}$`
+      );
+    } else {
+      console.log("Your balance is not enough to wirthdrow money");
+    }
+  }
+
+  deposit(enteredAmount) {
+    this.#amount = this.#amount + enteredAmount;
+    console.log(
+      `Success! You have deposited +${enteredAmount}$, and your balance is ${this.#amount}$`
+    );
+  }
+
+  details() {
+    console.log(`Hello dear ${this.name}. Your balance is ${this.#amount}$`);
+    console.log(`Your account id is: ${this.#account_id}`);
+  }
+
+  static tellMeAboutClass(){
+    console.log("This class is for creating accounts");
+  }
+
+  static time(){
+    const time = moment().format("YYYY MM DD hh:mm:ss");
+    console.log("Date:", time);
+  }
+}
+
+module.exports = Account
